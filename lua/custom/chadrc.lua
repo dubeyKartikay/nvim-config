b545eea618ac25ccabaad84ec7ecd4a5c617f3b0
Kartikay Dubey <kartikayd2003@gmail.com>
2024-05-09 18:25:10 +0530

    transprency stuff

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -7,7 +7,7 @@ local highlights = require "custom.highlights"
 M.ui = {
   theme = "tokyodark",
   theme_toggle = { "tokyodark", "one_light" },
-
+  transparency = true,
   hl_override = highlights.override,
   hl_add = highlights.add,
   tabufline ={
--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -6,5 +6,8 @@
 --   command = "tabdo wincmd =",
 vim.opt.mouse = ""
 vim.wo.relativenumber = true
---
--- })
+-- Function to set the terminal window title to the current file names
+
+vim.cmd([[autocmd BufEnter * let &titlestring = expand("%:t")]])
+
+vim.api.nvim_command('set title')
